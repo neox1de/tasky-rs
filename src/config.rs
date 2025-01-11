6d13a89e0efa10d9fb9ef06a4962a0c01b7e17d0
Neoxx1de Moharami <neox1de@proton.me>
2025-01-11 12:14:00 +0330

    chore: update dependencies and clean up code

--- src/config.rs
+++ src/config.rs
@@ -13,42 +13,81 @@ const ASCII_BANNER: &str = r#"
 
 pub fn build_app() -> Command {
     Command::new("tasky")
-        .bin_name("tasky")
-        .version("0.1.0")
+        .version(env!("CARGO_PKG_VERSION"))
         .about("A todo(task) manager written in Rust")
         .before_help(ASCII_BANNER)
         .subcommand(Command::new("list").about("List all tasks"))
-        .subcommand(Command::new("add")
-            .about("Add a new task")
-            .arg(Arg::new("TITLE").required(true).help("The title of the task"))
-            .arg(Arg::new("DESCRIPTION").required(true).help("The description of the task"))
-            .arg(Arg::new("STATUS").required(false).help("The status of the task (default: To-Do)")))
-        .subcommand(Command::new("remove")
-            .about("Remove a task by ID")
-            .arg(Arg::new("ID").required(true).help("The ID of the task to remove")))
-        .subcommand(Command::new("edit")
-            .about("Edit a task by ID")
-            .arg(Arg::new("ID").required(true).help("The ID of the task to edit"))
-            .arg(Arg::new("FIELD").required(true).value_parser(["title", "description", "status"]).help("The field to edit"))
-            .arg(Arg::new("VALUE").required(true).help("The new value for the field")))
-        .subcommand(Command::new("show")
-            .about("Show details of a task by ID")
-            .arg(Arg::new("ID").required(true).help("The ID of the task to show")))
-        .subcommand(Command::new("set")
-            .about("Set the status of a task by ID")
-            .arg(Arg::new("ID").required(true).help("The ID of the task"))
-            .arg(Arg::new("STATUS").required(true).help("The new status of the task")))
+        .subcommand(
+            Command::new("add")
+                .about("Add a new task")
+                .arg(
+                    Arg::new("TITLE")
+                        .required(true)
+                        .help("The title of the task"),
+                )
+                .arg(
+                    Arg::new("DESCRIPTION")
+                        .required(true)
+                        .help("The description of the task"),
+                )
+                .arg(
+                    Arg::new("STATUS")
+                        .required(false)
+                        .value_parser(["To-Do", "In-Progress", "Done", "Deferred", "Cancelled"])
+                        .help("The status of the task (default: To-Do)"),
+                ),
+        )
+        .subcommand(
+            Command::new("remove").about("Remove a task by ID").arg(
+                Arg::new("ID")
+                    .required(true)
+                    .help("The ID of the task to remove"),
+            ),
+        )
+        .subcommand(
+            Command::new("edit")
+                .about("Edit a task by ID")
+                .arg(
+                    Arg::new("ID")
+                        .required(true)
+                        .help("The ID of the task to edit"),
+                )
+                .arg(
+                    Arg::new("FIELD")
+                        .required(true)
+                        .value_parser(["title", "description", "status"])
+                        .help("The field to edit"),
+                )
+                .arg(
+                    Arg::new("VALUE")
+                        .required(true)
+                        .help("The new value for the field"),
+                ),
+        )
+        .subcommand(
+            Command::new("show")
+                .about("Show details of a task by ID")
+                .arg(
+                    Arg::new("ID")
+                        .required(true)
+                        .help("The ID of the task to show"),
+                ),
+        )
+        .subcommand(
+            Command::new("set")
+                .about("Set the status of a task by ID")
+                .arg(Arg::new("ID").required(true).help("The ID of the task"))
+                .arg(
+                    Arg::new("STATUS")
+                        .required(true)
+                        .value_parser(["To-Do", "In-Progress", "Done", "Deferred", "Cancelled"])
+                        .help("The status of the task"),
+                ),
+        )
 }
 
 pub fn get_data_dir() -> PathBuf {
-    let dir = if cfg!(target_os = "linux") {
-        dirs::data_local_dir().unwrap().join("tasky-rs")
-    } else if cfg!(target_os = "macos") {
-        dirs::data_dir().unwrap().join("tasky-rs")
-    } else if cfg!(target_os = "windows") {
-        dirs::data_dir().unwrap().join("tasky-rs")
-    } else {
-        panic!("Unsupported OS");
-    };
-    dir
+    dirs::data_dir()
+        .unwrap()
+        .join("tasky-rs")
 }
--- src/main.rs
+++ src/main.rs
@@ -22,33 +22,36 @@ fn main() {
         Some(("add", sub_m)) => {
             let title = sub_m.get_one::<String>("TITLE").unwrap();
             let description = sub_m.get_one::<String>("DESCRIPTION").unwrap();
-            let status = sub_m.get_one::<String>("STATUS").map(|s| s.as_str()).unwrap_or("To-Do");
+            let status = sub_m
+                .get_one::<String>("STATUS")
+                .map(|s| s.as_str())
+                .unwrap_or("To-Do");
             commands::add::execute(title, description, status);
-        },
+        }
         Some(("remove", sub_m)) => {
             let id = sub_m.get_one::<String>("ID").unwrap();
             commands::remove::execute(id);
-        },
+        }
         Some(("edit", sub_m)) => {
             let id = sub_m.get_one::<String>("ID").unwrap();
             let field = sub_m.get_one::<String>("FIELD").unwrap();
             let value = sub_m.get_one::<String>("VALUE").unwrap();
             commands::edit::execute(id, field, value);
-        },
+        }
         Some(("show", sub_m)) => {
             let id = sub_m.get_one::<String>("ID").unwrap();
             commands::show::execute(id);
-        },
+        }
         Some(("set", sub_m)) => {
             let id = sub_m.get_one::<String>("ID").unwrap();
             let status = sub_m.get_one::<String>("STATUS").unwrap();
             commands::set::execute(id, status);
-        },
+        }
         None => {
             println!("Missing argument, use --help to get help.");
-        },
+        }
         _ => {
             let _ = config::build_app().print_help();
-        },
+        }
     }
 }
